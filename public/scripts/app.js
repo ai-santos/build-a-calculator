4991bc34c77fc7fabf0028c18b9b62719f28bdc1
Aileen <asantos3026@gmail.com>
2017-03-01 16:02:24 -0800

    Decimal, percent, positive & negative sign all work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,101 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var calc = new Calculator()
-  var calcButtons = document.getElementsByClassName('calc-display-num')[0]
+  var calc = new Calculator();
+  var calcButtons = document.getElementsByClassName('calc-display-num')[0];
   calcButtons.addEventListener('click', function(event) {
-    calc.displayNums(event)
+    calc.buttonPressed(event);
   })
 })
 
 function Calculator() {
+  this.decimalCheck = false;
   this.firstNums = ''
+  this.secondNums = ''
+  // this.operator = ''
 }
 
-Calculator.prototype.displayNums = function(event) {
-  this.firstNums += event.target.innerText
-  console.log(this.firstNums)
-  var calcWindow = document.getElementsByClassName('calc-display-window')[0]
-  calcWindow.innerText = this.firstNums;
+Calculator.prototype.displayNums = function(innerText) {
+  this.firstNums += innerText;
+  this.updateDisplay();
 }
 
 // calculator match function
-Calculator.prototype.
-// conditional if match % call percent function
+Calculator.prototype.buttonPressed = function(event) {
+  switch(event.target.innerText) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      this.displayNums(event.target.innerText);
+      break;
+    case '.':
+      this.addDecimal();
+      break;
+    case '%':
+      this.calculatePercent();
+      break;
+    case 'C':
+      this.clearDisplay();
+      break;
+    case '+/-':
+      this.posNeg();
+      break;
+    // case '+':
+    //   this.addNums();
+    //   break;
+  }
+}
 
+Calculator.prototype.addDecimal = function() {
+  if(!this.decimalCheck) {
+    this.decimalCheck = true;
+    this.displayNums('.');
+  }
+}
+
+Calculator.prototype.calculatePercent = function() {
+  this.firstNums /= 100
+  this.updateDisplay();
+}
+
+Calculator.prototype.updateDisplay = function() {
+  var calcWindow = document.getElementsByClassName('calc-display-window')[0];
+  calcWindow.innerText = this.firstNums;
+}
+
+Calculator.prototype.clearDisplay = function() {
+  this.decimalCheck = false;
+  this.firstNums = '0';
+  this.secondNums = '';
+  this.updateDisplay();
+}
+
+Calculator.prototype.posNeg = function() {
+  this.firstNums *= -1;
+  this.updateDisplay()
+}
+
+// Calculator.prototype.addNums = function() {
+
+// }
+
+// Calculator.prototype.resolveOperation = function() {
+
+// }
+
+// conditional if match % call percent function
 // calculator percent function
 // take num and divide by 100
+
+
+
+
+
 
 
   // function toggleColor(event) {
